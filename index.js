9836619598f34b93e7de72c4d87e87d18f35a4dd
Bara Borneo <barared28@gmail.com>
2022-05-14 14:16:35 +0700

    cors

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://wa-gateway-client.vercel.app",
-    },
+        origin: "https://wa-gateway-client.vercel.app/"
+    }
 });
 
 // handlers import
